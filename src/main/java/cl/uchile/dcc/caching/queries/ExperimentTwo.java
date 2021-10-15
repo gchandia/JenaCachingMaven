2cb1659124c8d8b89e3a19507e941b3c85b5ea2e
gchandia <gch120441@gmail.com>
2021-10-14 23:54:09 -0300

    Added ExperimentOne

---
+++ src/main/java/cl/uchile/dcc/caching/queries/ExperimentTwo.java
@@ -0,0 +1,63 @@
+package cl.uchile.dcc.caching.queries;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.sparql.algebra.Algebra;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.engine.QueryIterator;
+import org.apache.jena.tdb.TDBFactory;
+
+public class ExperimentTwo {
+	public static void main(String[] args) {
+		String s1 = "SELECT ?var 1 WHERE {?var2 <http://www.wikidata.org/prop/direct/P40>  <http://www.wikidata.org/entity/Q76>; \n"
+				+ "?var1 <http://www.wikidata.org/entity/Q186443532> }";
+		String s2 = "SELECT ?var1 WHERE { <http://www.wikidata.org/entity/Q501128> ?var1  ?var2}";
+		String s3 = "SELECT  * WHERE { ?var1  <http://www.wikidata.org/prop/direct/P31>  <http://www.wikidata.org/entity/Q6256> ; "
+				+ "<http://www.wikidata.org/prop/direct/P1566>  \"3175395\" }";
+		String s4 = "SELECT  * WHERE { ?var1  <http://www.wikidata.org/prop/direct/P31>  <http://www.wikidata.org/entity/Q6256> ;"
+				+ " <http://www.wikidata.org/prop/direct/P1566>  \"2589581\" }";
+		String s5 = "SELECT  ?var1 ?var1Label ?var2 ?var2Label WHERE { ?var1  <http://www.wikidata.org/prop/direct/P586>  ?var3 }";
+		String s6 = "SELECT DISTINCT  ?var1 WHERE { ?var1  <http://www.wikidata.org/prop/direct/P31>  <http://www.wikidata.org/entity/Q571> ; "
+				+ "<http://www.wikidata.org/prop/direct/P364>  ?var2 ; <http://www.wikidata.org/prop/direct/P407>  ?var3 FILTER ( ?var2 != ?var3 )}";
+		String s7 = "SELECT  ?var1 ?var2 WHERE { <http://www.wikidata.org/entity/Q25287> <http://www.wikidata.org/prop/direct/P625>  ?var1 "
+				+ "OPTIONAL { <http://www.wikidata.org/entity/Q25287> <http://www.wikidata.org/prop/direct/P131>  ?var3 . "
+				+ "?var3 <http://www.wikidata.org/prop/direct/P625>  ?var2}}";
+		String s8 = "SELECT  ?var1 ?var2 WHERE { <http://www.wikidata.org/entity/Q417743> ?var1  ?var2}";
+		String s9 = "SELECT  ?var1 ?var2 WHERE { <http://www.wikidata.org/entity/Q416426> ?var1  ?var2}";
+		
+		Query q1 = QueryFactory.create(s8);
+		String myModel = "D:\\tmp\\WikiDB";
+		Dataset ds = TDBFactory.createDataset(myModel);
+		// Write if I wanna write, but I'll be using read to query over it mostly
+		ds.begin(ReadWrite.READ);
+		
+		// Define model and Query
+		final Model model = ds.getDefaultModel();
+		QueryExecution q11Exec = QueryExecutionFactory.create(q1, model);
+		ResultSet q11Results = q11Exec.execSelect();
+		Op op = Algebra.compile(q1);
+		op = Algebra.optimize(op);
+		QueryIterator cache_qit = Algebra.exec(op, model);
+		int resultAmount = 0;
+		int cacheResultAmount = 0;
+		
+		while (q11Results.hasNext()) {
+			q11Results.next();
+			resultAmount++;
+		}
+		
+		while (cache_qit.hasNext()) {
+			cache_qit.next();
+			cacheResultAmount++;
+		}
+		
+		System.out.println(resultAmount);
+		System.out.println(cacheResultAmount);
+	}
+}
