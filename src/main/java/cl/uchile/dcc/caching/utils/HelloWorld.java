0bf92de528e040e69185eb952b4d74ab64ebd8cd
gchandia <gch120441@gmail.com>
2023-04-09 15:37:00 -0400

    Fixed file creation
    Fixed file creation

--- src/main/java/cl/uchile/dcc/caching/utils/HelloWorld.java
+++ src/main/java/cl/uchile/dcc/caching/utils/HelloWorld.java
@@ -1,9 +1,11 @@
 package cl.uchile.dcc.caching.utils;
 
 public class HelloWorld {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-
+  public static void main(String[] args) {
+	System.out.println("Hello World");
+	String s = "/home/gchandia/wikidata_logs/FilteredLogs_1.tsv";
+	int ss = s.indexOf("F");
+	String sss = s.substring(s.indexOf("F"), s.length());
+	System.out.println(sss);
+  }
 }
