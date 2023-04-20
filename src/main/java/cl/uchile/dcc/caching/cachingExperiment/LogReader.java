465caff00cf03338b26d62240cbd0a5fdfd6beb4
gchandia <gch120441@gmail.com>
2023-04-19 22:50:10 -0400

    THIS IS IT
    First 50K queries

--- src/main/java/cl/uchile/dcc/caching/cachingExperiment/LogReader.java
+++ src/main/java/cl/uchile/dcc/caching/cachingExperiment/LogReader.java
@@ -393,12 +393,11 @@ public class LogReader {
   public void readLog(File file) throws IOException {
 	InputStream is = new FileInputStream(file);
 	final Scanner sc = new Scanner(is);
-	System.out.println(myBgpSubQueries.size());
 	final PrintWriter w = new PrintWriter(new FileWriter("/home/gchandia/Thesis/" 
 														 + file.getName().substring(file.getName().indexOf("F"), file.getName().length()) 
 														 + "_Results.txt"));
 	
-    for (int i = 1; i <= 1; i++) {
+    for (int i = 1; i <= 50000; i++) {
       final Runnable stuffToDo = new Thread() {
         @Override
         public void run() {
