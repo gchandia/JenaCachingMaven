d8fa3c536d25a0ce41816b05337caa7adc9863b5
gchandia <gch120441@gmail.com>
2023-05-16 10:52:35 -0400

    Again
    Again

--- src/main/java/cl/uchile/dcc/caching/cachingExperiment/LogReader.java
+++ src/main/java/cl/uchile/dcc/caching/cachingExperiment/LogReader.java
@@ -397,10 +397,10 @@ public class LogReader {
 	final Scanner sc = new Scanner(is);
 	final PrintWriter w = new PrintWriter(new FileWriter("/home/gchandia/Thesis/" 
 														 + file.getName().substring(file.getName().indexOf("F"), file.getName().length()) 
-														 + "_Results.txt"));
+														 + "_LRU_Results.txt"));
 	final PrintWriter ww = new PrintWriter(new FileWriter("/home/gchandia/Thesis/"
 														 + file.getName().substring(file.getName().indexOf("F"), file.getName().length())
-														 + "_Errors.txt"));
+														 + "_LRU_Errors.txt"));
 	
     for (int i = 1; i <= 10000; i++) {
       final Runnable stuffToDo = new Thread() {
